0d1ea21b260aac76fb8d0cbfc011f5dcf8c408f0
Tyler Hunt <tyhunt04@gmail.com>
2019-04-09 20:55:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,13 @@ module FormsLab
     end
 
     post '/pirates' do
+      @pirates = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+     
       erb :'pirates/show'
     end
 
